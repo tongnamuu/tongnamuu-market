ab9581b966f598fc383334582bfe493f686a5a91
tongnamuu <thban33@naver.com>
2022-07-06 01:11:45 +0900

    flex

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,12 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (<div className='bg-red-100'>
-    <h1 className='text-blue-500'>tailwind css</h1>
-  </div> )
+  return <div className='bg-slate-400 py-10 flex flex-col space-y-5'>
+    <div className='bg-white p-10 rounded-2xl'></div>
+    <div className='bg-white p-10 rounded-2xl'></div>
+    <div className='bg-white p-10 rounded-2xl'></div>
+    <div className='bg-white p-10 rounded-2xl'></div>
+  </div>
 }
 
 export default Home
